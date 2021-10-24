274c12b0943c88ab0e64b134d598f122a3747149
Marlon <Marlonnlmx@outlook.com>
2021-10-24 11:35:38 -0300

    Making some changes

--- Projeto-Node/src/middleware/ensureAuthenticated.ts
+++ Projeto-Node/src/middleware/ensureAuthenticated.ts
@@ -2,28 +2,35 @@ import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
 interface IPayload {
-    sub: string;
+  sub: string;
 }
 
-export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
-    const authToken = request.headers.authorization;
+export function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authToken = request.headers.authorization;
 
-    if(!authToken){
-        return response.status(401).json({
-            errorCode: "token.invalid",
-        });
-    }
+  if (!authToken) {
+    return response.status(401).json({
+      errorCode: "token.invalid",
+    });
+  }
 
-    const [,token] = authToken.split("")
+  //Bearer 8934589345djisdjfk834u25ndsfksdkf
+  // [0] Bearer
+  // [1] 8934589345djisdjfk834u25ndsfksdkf
 
-    const { sub } = verify(token, process.env.JWT_SECRET)
+  const [, token] = authToken.split(" ");
 
-    try {
-        const { sub } = verify(token, process.env.JWT_SECRET) as IPayload
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
 
-        request.user_id = sub
-        return next();
-    } catch (error) {
-        return response.status(401).json({ errorCode: "token.expired"})
-    }
+    request.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).json({ errorCode: "token.expired" });
+  }
 }
